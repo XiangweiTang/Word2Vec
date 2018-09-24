0911916e470aa331d3023d633d6edc07359a698c
Xiangwei Tang <tangxiangwei@gmail.com>
2018-09-24 19:40:27 +0800

    Update the restore.

--- RestoreModel.py
+++ RestoreModel.py
@@ -6,4 +6,9 @@ sess=tf.Session()
 saver=tf.train.import_meta_graph('log\\model.ckpt.meta')
 saver.restore(sess,tf.train.latest_checkpoint('./log/'))
 
-v1=tf.get_variable("")
+#v0=tf.initialize_variables;
+
+v=tf.global_variables(scope="weight")
+
+v1=tf.get_variable("Variable:0",shape=[50000,128])
+print("")
